ac45173aabbf547454026aabb8567c9f22ff55d9
manuinamazon-web <manuinamazon@gmail.com>
2026-03-03 13:08:24 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://fegpsuxxtsyujnwvpovf.supabase.co'
-const supabaseKey = 'sb_publishable_eTvGlQ1Db-QMN4K5AotJJQ_OUSDpIbK'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlZ3BzdXh4dHN5dWpud3Zwb3ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MTk5OTgsImV4cCI6MjA4Nzk5NTk5OH0.RXUVl90XuB3k2ZsRhuLcWrvkU74hq3ekwBYzmQXOqBw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
